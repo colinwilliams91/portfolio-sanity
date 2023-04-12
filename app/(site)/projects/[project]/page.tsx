2e862b21c18509c5004f1f2f14ceb960cfae0c71
colin <colin.williams91@gmail.com>
2023-04-11 19:38:06 -0500

    install tailwind-prose, handled vanilla markdown styling from sanity studio blocks

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -26,22 +26,27 @@ const Project = async ({ params }: Props) => {
           View Project
         </a>
       </header>
+      {/* content goes here */}
+      <div className="text-lg text-gray-300 mt-5">
+        <PortableText value={project.content} />
+      </div>
       {/* image goes here */}
       <Image
         src={project.image}
         alt={project.name}
         width={1920}
         height={1080}
-        className="mt-10 border-2 border-gray-400 object-cover rounded-xl"
+        className="mt-5 border-2 border-gray-400 object-cover rounded-xl"
       />
-      {/* content goes here */}
-      <div className="text-lg text-gray-400 mt-5">
-        <PortableText value={project.content} />
-      </div>
       {/* contributions go here */}
-      <div className="text-lg text-gray-400 mt-5">
+      {/* <div className="flex"> */}
+      <h2 className="text-lg flex justify-center mt-5 mx-auto text-gray-300">
+        <b>Contributions</b>
+      </h2>
+      <article className="prose prose-xl prose-strong:font-bold prose-strong:text-gray-400 text-gray-400 mt-3 mx-auto">
         <PortableText value={project.contributions} />
-      </div>
+      </article>
+      {/* </div> */}
     </div>
   );
 };
