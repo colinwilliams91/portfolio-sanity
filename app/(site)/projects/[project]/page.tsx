4b731fc5c961fa8e25c9389852617a398f287273
colin <colin.williams91@gmail.com>
2023-04-14 22:15:37 -0500

    iframe sandboxed script blocked for gif project

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -41,8 +41,6 @@ const Project = async ({ params }: Props) => {
         width={1920}
         height={1080}
         className="mt-5 border-2 border-gray-400 object-cover rounded-xl"
-        unoptimized={true}
-        allow-scripts={true}
       />
       {/* contributions go here */}
       {/* <div className="flex"> */}
