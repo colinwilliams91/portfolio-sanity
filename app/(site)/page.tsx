8186d2424addf395a6313dd416e567ce71fd68b8
colin <colin.williams91@gmail.com>
2023-05-05 12:34:13 -0500

    cursorX mouse track color

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import HomeHeadshot from "../components/HomeHeadshot";
+import L from "../components/Letters";
 
 // const inter = Inter({ subsets: ["latin"] });
 {
@@ -15,19 +16,28 @@ const Home = async () => {
 
   return (
     <div>
-      <h1 className="font-extralight text-7xl mt-12">
+      <h1 className="font-light text-7xl mt-12">
         Hello I&apos;m{" "}
-        <span className=" font-bold bg-gradient-to-r from-orange-400 to via-red-500 bg-purple-600 bg-clip-text text-transparent">
+        <span className="font-bold bg-gradient-to-r from-orange-400 to via-red-500 bg-purple-600 bg-clip-text text-transparent">
           Colin!
         </span>
       </h1>
-      {/* <p className="mt-3 text-xl text-gray-100">
+      <p className="mt-3 text-xl text-gray-100 ml-[5px]">
         Thanks for visiting my portfolio site!
-      </p> */}
-      <p className="flex justify-center mt-24 text-xl text-gray-100">
-        べ &nbsp; P r o j e c t s &nbsp; べ
       </p>
-      <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <p className="flex justify-center mt-20 mb-4 text-xl text-gray-100">
+        <L el={"べ"} />
+        <L el={"P"} />
+        <L el={"r"} />
+        <L el={"o"} />
+        <L el={"j"} />
+        <L el={"e"} />
+        <L el={"c"} />
+        <L el={"t"} />
+        <L el={"s"} />
+        <L el={"べ"} />
+      </p>
+      <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {projects.map((project) => (
           <Link
             href={`/projects/${project.slug}`}
---
+++ app/components/Letters.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import { useEffect } from "react";
+
+interface LetterProps {
+  el: string;
+}
+
+const L: any = ({ el }: LetterProps) => {
+  const cursorX = useMotionValue(0);
+  const cursorY = useMotionValue(0);
+
+  function handleMouseMove(event: MouseEvent) {
+    cursorX.set(event.clientX);
+    cursorY.set(event.clientY);
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <motion.span
+      style={{
+        color: useTransform(
+          cursorX,
+          [0, 1000, 2000],
+          ["#ff6600", "#e100ff", "#02ecfd"]
+        ),
+        boxShadow: "0px 1px 2px 0px",
+      }}
+      whileHover={{ scale: 1.2 }}
+      whileTap={{ scale: 0.9 }}
+      transition={{ type: "spring", stiffness: 666, damping: 6 }}
+    >
+      &nbsp;{el}&nbsp;
+    </motion.span>
+  );
+};
+
+export default L;
