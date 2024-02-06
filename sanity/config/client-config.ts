974411ef0b5f78adf1682795365d21e66dc8d4d4
colin <colin.williams.dev@gmail.com>
2024-02-05 19:15:57 -0600

    fix!: apply schemas to sanity config for type safety

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -1,8 +1,13 @@
+import schemas from "../schemas";
+
 const config = {
   projectId: 'coo17utp',
   dataset: 'production',
   useCdn: false,
   apiVersion: '2024-02-05',
+  schema: {
+    types: schemas
+  }
 }
 
 export default config;
