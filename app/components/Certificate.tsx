37164384b54afaa35d1ec8a91391f7d47bcc5a75
colin <colin.williams.dev@gmail.com>
2024-02-07 13:12:13 -0600

    style!: match project card for cert card, add fn to show hover tooltip not working

--- app/components/Certificate.tsx
+++ app/components/Certificate.tsx
@@ -7,6 +7,16 @@ import Link from "next/link";
 
 const Certificate: any = (prop: any) => {
   const [isHovered, setIsHovered] = useState(false);
+  console.log("PROP IN CERT COMPONENT:", prop);
+
+  const showToolTip = (text: string) => {
+    console.log("cert hovered", text);
+    return (
+      <div className="absolute bg-gray-800 text-white p-2 rounded shadow-md opacity-0 hover:opacity-100 transition-opacity duration-300">
+        {text}
+      </div>
+    )
+  };
 
   /* TODO: set <iframe> to Click, style to center, change hosted images to pdf host like Cloud Academy Cert */
 
@@ -17,7 +27,7 @@ const Certificate: any = (prop: any) => {
         // onMouseEnter={() => setIsHovered(true)}
         // onMouseLeave={() => setIsHovered(false)}
         whileHover={{ scale: 1.15 }}
-        onHoverStart={() => setIsHovered(true)}
+        onHoverStart={() => { setIsHovered(true); }}
         onHoverEnd={() => setIsHovered(false)}
         whileTap={{ scale: 0.97 }}
         transition={{ type: "spring", stiffness: 400, damping: 13 }}
@@ -38,8 +48,8 @@ const Certificate: any = (prop: any) => {
             alt={prop.slug}
             width={234}
             height={171}
-            className="border-[3px] border-double p-1
-            rounded-xl mt-10 h-[171px] w-[234px]
+            className="border-[2px] p-1
+            rounded-2xl mt-10 h-[171px] w-[234px]
             bg-orange-400/[17%] border-violet-400
             hover:border-cyan-400 hover:bg-gradient-to-tr
             from-pink-500/40 to-cyan-300/30 transition
