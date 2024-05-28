2155dd0ff20294b319b8f3258a6548fe900b0d67
colin <colin.williams.dev@gmail.com>
2024-05-27 21:46:30 -0500

    style: correct border radius, made div into clickable Link href project

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -2,6 +2,7 @@ import { getProject } from "@/sanity/sanity-utils";
 import { PortableText } from "@portabletext/react";
 import Image from "next/image";
 import RightArrow from "../../../components/RightArrow";
+import Link from "next/link";
 
 type Props = {
   params: { project: string };
@@ -35,16 +36,26 @@ const Project = async ({ params }: Props) => {
         <PortableText value={project.content} />
       </div>
       {/* image goes here */}
-      <Image
-        src={project.image}
-        alt={project.name}
-        width={1920}
-        height={1080}
-        className="mt-5 border-[4px] border-double border-violet-300/90 object-cover rounded-xl"
-      />
+      <div className="flex">
+        <Link
+          href={project.url}
+          className="rounded-2xl p-1 bg-orange-400/[17%]
+          border-[2px] border-violet-400 my-5
+          hover:border-cyan-400 hover:bg-gradient-to-tr
+          from-pink-500/40 to-cyan-300/30 transition">
+          <Image
+            src={project.image}
+            alt={project.name}
+            width={1920}
+            height={1080}
+            className="object-cover rounded-xl border
+              border-violet-300 transition-shadow"
+          />
+        </Link>
+      </div>
       {/* contributions go here */}
       {/* <div className="flex"> */}
-      <h2 className="text-lg flex justify-center mt-5 mx-auto text-gray-200">
+      <h2 className="text-lg flex justify-center mt-2 mx-auto text-gray-200">
         <b>Contributions:</b>
       </h2>
       <article className="prose prose-xl prose-strong:font-bold prose-strong:text-gray-300 prose-a:text-gray-300 hover:prose-a:text-cyan-300 text-gray-300 mt-3 mx-auto">
